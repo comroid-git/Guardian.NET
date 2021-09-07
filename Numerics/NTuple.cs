c2ad58c9b902e4f17d718a7808ff836a82595f44
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-09-07 13:55:28 +0200

    introduce experimental CastTuple method

--- Numerics/NTuple.cs
+++ Numerics/NTuple.cs
@@ -12,5 +12,26 @@ namespace Guardian.Numerics
         public NTuple(int n) : base(true, n)
         {
         }
+
+        protected NTuple(bool mutable = true, params RefStack<TNum>[] stack) : base(mutable, stack)
+        {
+        }
+
+        public NTuple<TOut> CastTuple<TOut>() where TOut : unmanaged // todo: TEST!
+        {
+            var stacks = new List<RefStack<TOut>>();
+            for (int i = 0; i < Size; i++)
+            {
+                var i1 = i;
+                stacks.Add(new RefStack<TOut>(false) { Getter = () => (TOut)(object)this[i1] });
+            }
+
+            return new NTuple<TOut>(false, stacks.ToArray());
+        }
+
+        public static NTuple<TNum> operator +(NTuple<TNum> left, NTuple<TNum> right)
+        {
+            return left.Plus(right);
+        }
     }
 }
