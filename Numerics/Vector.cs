9dfa2205cfe4018f80aa43b41149e2921ae80e1a
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-09-07 13:35:59 +0200

    add Vector, Vector2 and Vector4 classes

--- Numerics/Vector.cs
+++ Numerics/Vector.cs
@@ -4,7 +4,7 @@ using Guardian.Mutatio;
 
 namespace Guardian.Numerics
 {
-    public class Vector3<TNum> : NTuple<TNum> where TNum : unmanaged
+    public abstract class Vector<TNum> : NTuple<TNum> where TNum : unmanaged
     {
         public const int IndexX = 0;
         public const int IndexY = 1;
@@ -32,8 +32,45 @@ namespace Guardian.Numerics
             set => StackZ.Set(value);
         }
 
+        protected Vector(int n) : base(n)
+        {
+        }
+    }
+
+    public class Vector2<TNum> : Vector<TNum> where TNum : unmanaged
+    {
+        public Vector2(TNum x, TNum y) : base(2)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class Vector3<TNum> : Vector<TNum> where TNum : unmanaged
+    {
         public Vector3(TNum x, TNum y, TNum z) : base(3)
         {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+
+    public class Vector4<TNum> : Vector<TNum> where TNum : unmanaged
+    {
+        public const int IndexW = 3;
+
+        public RefStack<TNum> StackW => Stack(IndexW);
+
+        public TNum W
+        {
+            get => StackW.Get();
+            set => StackW.Set(value);
+        }
+
+        public Vector4(TNum w, TNum x, TNum y, TNum z) : base(4)
+        {
+            W = w;
             X = x;
             Y = y;
             Z = z;
