4dcebc2b74af826f49d44e9956d305ba23a1da97
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-09-07 12:57:40 +0200

    introduce NTuple and Vector

---
+++ Numerics/NTuple.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Guardian.Mutatio;
+
+namespace Guardian.Numerics
+{
+    public class NTuple<TNum> : Reference<TNum> where TNum : unmanaged
+    {
+        public NTuple(int n) : base(true, n)
+        {
+        }
+    }
+}
---
+++ Numerics/Vector3.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+using Guardian.Mutatio;
+
+namespace Guardian.Numerics
+{
+    public class Vector3<TNum> : NTuple<TNum> where TNum : unmanaged
+    {
+        public const int IndexX = 0;
+        public const int IndexY = 1;
+        public const int IndexZ = 2;
+
+        public TNum X
+        {
+            get => Get(IndexX);
+            set => Set(IndexX, value);
+        }
+
+        public TNum Y
+        {
+            get => Get(IndexY);
+            set => Set(IndexY, value);
+        }
+
+        public TNum Z
+        {
+            get => Get(IndexZ);
+            set => Set(IndexZ, value);
+        }
+
+        public Vector3(TNum x, TNum y, TNum z) : base(3)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
