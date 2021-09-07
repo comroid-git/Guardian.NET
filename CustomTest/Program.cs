7cd2b66678577a09907e2b05b06cf048173dcdef
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-09-07 15:13:45 +0200

    Update Program.cs

--- CustomTest/Program.cs
+++ CustomTest/Program.cs
@@ -9,8 +9,8 @@ namespace CustomTest
         {
             var result = new Vector3<float>(0, 0, 0) + new Vector2<float>(1, 2);
             Console.WriteLine("First Result: " + result);
-            double[] resf = result.CastTuple<double>();
-            Console.WriteLine("Second Result: " + resf);
+            //double[] resf = result.CastTuple<double>();
+            //Console.WriteLine("Second Result: " + resf);
             var res2 = new QuaternionF(0, 0, 0, 0) + new QuaternionF(0, 0, 0, 0);
             Console.WriteLine("Third Result: " + res2);
         }
