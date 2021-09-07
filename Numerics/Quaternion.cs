590d4db875ec22dfea972a78ab02d701c68ef35b
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-09-07 16:01:23 +0200

    Update Quaternion.cs

--- Numerics/Quaternion.cs
+++ Numerics/Quaternion.cs
@@ -5,6 +5,18 @@ namespace Guardian.Numerics
 {
     public class Quaternion<TNum> : NTuple<TNum> where TNum : unmanaged
     {
+        public static Quaternion<TNum> EulerAngles(Vector3<TNum> euler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Quaternion<TNum> AxisRotation(Vector<TNum> axis, TNum angle)
+        {
+            var quaternion = axis.Normalize().Quaternion();
+            quaternion.W = angle;
+            return quaternion;
+        }
+
         public Quaternion(TNum x, TNum y, TNum z, TNum w) : base(4)
         {
             X = x;
@@ -58,27 +70,67 @@ namespace Guardian.Numerics
         #endregion
 
         #region Advanced Arithmetic Accessors
-
         /*
-        public Vector3 Forward() => new Vector3(
+        public Vector3<TNum> Forward() => new Vector3<TNum>(
             2 * (this.X * this.Z + this.W * this.Y),
             2 * (this.Y * this.Z - this.W * this.X),
             1 - 2 * (this.X * this.X + this.Y * this.Y)
         );
 
-        public Vector3 Up() => new Vector3(
+        public Vector3<TNum> Up() => new Vector3<TNum>(
             2 * (this.X * this.Y - this.W * this.Z),
             1 - 2 * (this.X * this.X + this.Z * this.Z),
             2 * (this.Y * this.Z + this.W * this.X)
         );
 
-        public Vector3 Left() => new Vector3(
+        public Vector3<TNum> Left() => new Vector3<TNum>(
             1 - 2 * (this.Y * this.Y + this.Z * this.Z),
             2 * (this.X * this.Y + this.W * this.Z),
             2 * (this.X * this.Z - this.W * this.Y)
         );
-        */
 
+        public Vector3<TNum> EulerAngles()
+        {
+            // https://stackoverflow.com/questions/11492299/quaternion-to-euler-angles-algorithm-how-to-convert-to-y-up-and-between-ha
+
+            // Store the Euler angles in radians
+            Vector3<TNum> pitchYawRoll = new Vector3<TNum>();
+
+            double sthisw = this.W * this.W;
+            double sthisx = this.X * this.X;
+            double sthisy = this.Y * this.Y;
+            double sthisz = this.Z * this.Z;
+
+            // If thisuaternion is normalised the unit is one, otherwise it is the correction factor
+            double unit = sthisx + sthisy + sthisz + sthisw;
+            double test = this.X * this.Y + this.Z * this.W;
+
+            if (test > 0.4999f * unit) // 0.4999f OR 0.5f - EPSILON
+            {
+                // Singularity at north pole
+                pitchYawRoll.Y = 2f * (float)Math.Atan2(this.X, this.W); // Yaw
+                pitchYawRoll.X = MathF.PI * 0.5f; // Pitch
+                pitchYawRoll.Z = 0f; // Roll
+                return pitchYawRoll;
+            }
+            else if (test < -0.4999f * unit) // -0.4999f OR -0.5f + EPSILON
+            {
+                // Singularity at south pole
+                pitchYawRoll.Y = -2f * (float)Math.Atan2(this.X, this.W); // Yaw
+                pitchYawRoll.X = -MathF.PI * 0.5f; // Pitch
+                pitchYawRoll.Z = 0f; // Roll
+                return pitchYawRoll;
+            }
+            else
+            {
+                pitchYawRoll.Y = (float)Math.Atan2(2f * this.X * this.W + 2f * this.Y * this.Z, 1 - 2f * (sthisz + sthisw)); // Yaw 
+                pitchYawRoll.X = (float)Math.Asin(2f * (this.X * this.Z - this.W * this.Y)); // Pitch 
+                pitchYawRoll.Z = (float)Math.Atan2(2f * this.X * this.Y + 2f * this.Z * this.W, 1 - 2f * (sthisy + sthisz)); // Roll 
+            }
+
+            return pitchYawRoll;
+        }
+        */
         #endregion
     }
 
